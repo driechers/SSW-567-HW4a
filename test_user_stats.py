63b5ddecfc67ceaad3ed109419eafd9a6af5984d
Daniel Riechers <driechers101@gmail.com>
2019-09-21 16:32:55 -0500

    Added tests and fixed app
    Signed-off-by: Daniel Riechers <driechers101@gmail.com>

--- test_user_stats.py
+++ test_user_stats.py
@@ -2,5 +2,30 @@ from user_stats import *
 
 import pytest
 
-def test_true():
-    assert True == True
+def test_num_commits_valid():
+    assert get_num_commits('driechers/2x2x2') == 2
+
+def test_num_commits_bad_repo():
+    assert get_num_commits('driechers/NotARepo') == 0
+
+def test_num_commits_bad_user():
+    assert get_num_commits('notareallyrealuser/NotARepo') == 0
+
+def test_stats_valid():
+    assert get_stats('driechers') == \
+"""Repo 2x2x2 Number of commits: 2
+Repo asciiquarium Number of commits: 12
+Repo bijou-snake Number of commits: 17
+Repo boofuzz Number of commits: 30
+Repo ctags Number of commits: 30
+Repo grub-invaders Number of commits: 22
+Repo invaders-before-grub Number of commits: 30
+Repo radascii Number of commits: 10
+Repo SSW-567 Number of commits: 1
+Repo SSW-567-HW01 Number of commits: 4
+Repo SSW-567-HW02a Number of commits: 7
+Repo SSW-567-HW4a Number of commits: 2
+"""
+
+def test_stats_bad_user():
+    assert get_stats('notareallyrealuser') == '\n'
--- user_stats.py
+++ user_stats.py
@@ -2,21 +2,33 @@
 
 import requests
 import json
+import sys
 
 def get_num_commits(full_name):
     response = requests.get('https://api.github.com/repos/%s/commits' % full_name)
-    commits = json.loads(response.text)
-    return len(commits)
+
+    if response.status_code == 200:
+        commits = json.loads(response.text)
+        return len(commits)
+    else:
+        return 0
 
 def get_stats(user):
     response = requests.get('https://api.github.com/users/%s/repos' % user)
-    r = ''
-    repos = json.loads(response.text)
-    for repo in repos:
-        r += 'Repo %s ' % repo['name']
-        r += 'Number of commits: %d\n' % get_num_commits(repo['full_name'])
 
-    return r
+    if response.status_code == 200:
+        r = ''
+        repos = json.loads(response.text)
+        for repo in repos:
+            r += 'Repo %s ' % repo['name']
+            r += 'Number of commits: %d\n' % get_num_commits(repo['full_name'])
+        return r
+    else:
+        return '\n'
 
 if __name__ == '__main__':
-    print(get_stats('driechers'), end='')
+    if len(sys.argv) != 2 or sys.argv[1] == '-h':
+        print('Usage:\n\t%s username' % sys.argv[0])
+        sys.exit(1)
+
+    print(get_stats(sys.argv[1]), end='')

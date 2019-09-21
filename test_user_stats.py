fe2bf90aacd59d31d000438575d8ffdcd05160dc
Daniel Riechers <driechers101@gmail.com>
2019-09-21 16:40:08 -0500

    Truncated full test to not include commits for this repo
    Signed-off-by: Daniel Riechers <driechers101@gmail.com>

--- test_user_stats.py
+++ test_user_stats.py
@@ -12,7 +12,8 @@ def test_num_commits_bad_user():
     assert get_num_commits('notareallyrealuser/NotARepo') == 0
 
 def test_stats_valid():
-    assert get_stats('driechers') == \
+    stats = get_stats('driechers')
+    assert stats[:-2] == \
 """Repo 2x2x2 Number of commits: 2
 Repo asciiquarium Number of commits: 12
 Repo bijou-snake Number of commits: 17
@@ -24,8 +25,7 @@ Repo radascii Number of commits: 10
 Repo SSW-567 Number of commits: 1
 Repo SSW-567-HW01 Number of commits: 4
 Repo SSW-567-HW02a Number of commits: 7
-Repo SSW-567-HW4a Number of commits: 2
-"""
+Repo SSW-567-HW4a Number of commits: """
 
 def test_stats_bad_user():
     assert get_stats('notareallyrealuser') == '\n'

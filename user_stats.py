32811e4076e31616c47171d42f108113de8d7c39
Daniel Riechers <driechers101@gmail.com>
2019-09-21 16:08:11 -0500

    Completed User Stats program
    Signed-off-by: Daniel Riechers <driechers101@gmail.com>

--- user_stats.py
+++ user_stats.py
@@ -1,7 +1,22 @@
 #!/usr/bin/python3
 
 import requests
+import json
+
+def get_num_commits(full_name):
+    response = requests.get('https://api.github.com/repos/%s/commits' % full_name)
+    commits = json.loads(response.text)
+    return len(commits)
+
+def get_stats(user):
+    response = requests.get('https://api.github.com/users/%s/repos' % user)
+    r = ''
+    repos = json.loads(response.text)
+    for repo in repos:
+        r += 'Repo %s ' % repo['name']
+        r += 'Number of commits: %d\n' % get_num_commits(repo['full_name'])
+
+    return r
 
 if __name__ == '__main__':
-    response = requests.get('https://api.github.com/users/driechers/repos')
-    print(response.text)
+    print(get_stats('driechers'), end='')
